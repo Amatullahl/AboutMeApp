9d5c6680543dbf95abe788173062081ccb78db6a
Amatullah <l.amatullah00@gmail.com>
2025-07-02 10:52:53 +0100

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Amatullah Lonandwala on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
